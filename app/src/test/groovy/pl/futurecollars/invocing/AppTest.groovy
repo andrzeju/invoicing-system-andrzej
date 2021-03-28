fff0b93641de8782232fd137e2b5ed0d94f9ff28
Andrzej <andrzej.urban.pl@gmail.com>
2021-03-28 13:16:35 +0200

    Add missing tests and fix lombok-jacoco integration

--- app/src/test/groovy/pl/futurecollars/invocing/AppTest.groovy
+++ app/src/test/groovy/pl/futurecollars/invocing/AppTest.groovy
@@ -6,16 +6,6 @@ package pl.futurecollars.invocing
 import spock.lang.Specification
 
 class AppTest extends Specification {
-    def "application has a greeting"() {
-        setup:
-        def app = new App()
-
-        when:
-        def result = app.greeting
-
-        then:
-        result != null
-    }
 
     def "dummy main method test"() {
         setup:
