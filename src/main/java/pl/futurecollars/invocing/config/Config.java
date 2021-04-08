76852bfc65bf6f80ab3f5a4d7ad4a145848eb59e
Andrzej <andrzej.urban.pl@gmail.com>
2021-04-08 19:18:55 +0200

    fixed typo in package name invocing -> invoicing

--- src/main/java/pl/futurecollars/invocing/config/Config.java
+++
@@ -1,7 +0,0 @@
-package pl.futurecollars.invocing.config;
-
-public class Config {
-
-    public static final String DATABASE_LOCATION = "db/invoices.txt";
-    public static final String ID_FILE_LOCATION = "db/id.txt";
-}
