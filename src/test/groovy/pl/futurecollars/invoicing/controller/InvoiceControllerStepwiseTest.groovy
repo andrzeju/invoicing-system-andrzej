a55e9d02dedb20c0c19e378e93a82d3ab7925407
Andrzej <andrzej.urban.pl@gmail.com>
2021-05-08 10:42:26 +0200

    force new spring boot test context - stepwise test in parallel might be failing without it

--- src/test/groovy/pl/futurecollars/invoicing/controller/InvoiceControllerStepwiseTest.groovy
+++ src/test/groovy/pl/futurecollars/invoicing/controller/InvoiceControllerStepwiseTest.groovy
@@ -19,7 +19,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print
 
 @AutoConfigureMockMvc
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Stepwise
 class InvoiceControllerStepwiseTest extends Specification {
 
