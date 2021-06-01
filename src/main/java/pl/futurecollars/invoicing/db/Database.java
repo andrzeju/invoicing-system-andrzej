3ec31747467682a2f0f3dd1b5aebd8b010e7d7cd
Andrzej <andrzej.urban.pl@gmail.com>
2021-06-01 10:10:29 +0200

    refactor to generic database

--- src/main/java/pl/futurecollars/invoicing/db/Database.java
+++ src/main/java/pl/futurecollars/invoicing/db/Database.java
@@ -1,36 +1,21 @@
 package pl.futurecollars.invoicing.db;
 
-import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
-import java.util.function.Function;
-import java.util.function.Predicate;
-import pl.futurecollars.invoicing.model.Invoice;
-import pl.futurecollars.invoicing.model.InvoiceEntry;
 
-public interface Database {
+public interface Database<T extends WithId> {
 
-    long save(Invoice invoice);
+    long save(T item);
 
-    Optional<Invoice> getById(long id);
+    Optional<T> getById(long id);
 
-    List<Invoice> getAll();
+    List<T> getAll();
 
-    Optional<Invoice> update(long id, Invoice updatedInvoice);
+    Optional<T> update(long id, T updatedItem);
 
-    Optional<Invoice> delete(long id);
+    Optional<T> delete(long id);
 
     default void reset() {
-        getAll().forEach(invoice -> delete(invoice.getId()));
+        getAll().forEach(item -> delete(item.getId()));
     }
-
-    default BigDecimal visit(Function<InvoiceEntry, BigDecimal> amountToSum,
-                             Predicate<Invoice> invoiceSourcePredicate) {
-        return getAll().stream()
-            .filter(invoiceSourcePredicate)
-            .flatMap(inv -> inv.getEntries().stream())
-            .map(amountToSum)
-            .reduce(BigDecimal.ZERO, BigDecimal::add);
-    }
-
 }
---
+++ src/main/java/pl/futurecollars/invoicing/db/WithId.java
@@ -0,0 +1,9 @@
+package pl.futurecollars.invoicing.db;
+
+public interface WithId {
+
+    Long getId();
+
+    void setId(Long id);
+
+}
