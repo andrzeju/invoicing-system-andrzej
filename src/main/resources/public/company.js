ba6516f52e376768201c44320d8db0618eff21bb
Andrzej <andrzej.urban.pl@gmail.com>
2021-06-10 07:05:36 +0200

    added bootstrap and new company form

--- src/main/resources/public/company.js
+++ src/main/resources/public/company.js
@@ -1,27 +1,56 @@
-const loadCompanies = async() => {
-            const response = await fetch('http://localhost:7766/companies');
-            const companies = await response.json();
+const loadCompanies = async () => {
+    const response = await fetch('http://localhost:7766/companies');
+    const companies = await response.json();
 
-            let companiesTable = document.getElementById("companyTable")
+    const companiesTable = document.getElementById("companiesTable")
+    companies.forEach(company => {
+        const row = companiesTable.insertRow(-1);
 
-            companies.forEach(company => {
-                let row = companiesTable.insertRow(-1);
+        const taxIdCell = row.insertCell(0);
+        taxIdCell.innerText = company.taxIdentificationNumber;
 
-                let tinCell = row.insertCell(0);
-                tinCell.innerText = company.taxIdentificationNumber;
+        const addressCell = row.insertCell(1);
+        addressCell.innerText = company.address;
 
-                let addressCell = row.insertCell(1);
-                addressCell.innerText = company.address;
+        const nameCell = row.insertCell(2);
+        nameCell.innerText = company.name;
 
-                let nameCell = row.insertCell(2);
-                nameCell.innerText = company.name;
+        const pensionInsuranceCell = row.insertCell(3);
+        pensionInsuranceCell.innerText = company.pensionInsurance;
 
-                let pensionInsuranceCell = row.insertCell(3);
-                pensionInsuranceCell.innerText = company.pensionInsurance;
-
-                let healthInsuranceCell = row.insertCell(4);
-                healthInsuranceCell.innerText = company.healthInsurance;
-            })
+        const healthInsuranceCell = row.insertCell(4);
+        healthInsuranceCell.innerText = company.healthInsurance;
+    })
 }
 
-loadCompanies();
+const serializeFormToJson = form => JSON.stringify(
+    Array.from(new FormData(form).entries())
+        .reduce((m, [key, value]) =>
+            Object.assign(m, {[key]: value}), {})
+);
+
+function handleAddCompanyFormSubmit() {
+    const form = $("#addCompanyForm");
+    form.on('submit', function (e) {
+        e.preventDefault();
+
+        $.ajax({
+            url: 'companies',
+            type: 'post',
+            contentType: 'application/json',
+            data: serializeFormToJson(this),
+            success: function (data) {
+                $("#companiesTable").find("tr:gt(0)").remove();
+                loadCompanies()
+            },
+            error: function (jqXhr, textStatus, errorThrown) {
+                alert(errorThrown)
+            }
+        });
+    });
+}
+
+window.onload = function () {
+    loadCompanies();
+    handleAddCompanyFormSubmit()
+};
