3f0fa339dfd71867e37d28f121299b1cb5424769
Andrzej <andrzej.urban.pl@gmail.com>
2021-06-23 07:08:22 +0200

    angular app now works with spring security

--- src/main/java/pl/futurecollars/invoicing/WebSecurityConfig.java
+++ src/main/java/pl/futurecollars/invoicing/WebSecurityConfig.java
@@ -1,16 +1,33 @@
 package pl.futurecollars.invoicing;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.access.channel.ChannelProcessingFilter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import pl.futurecollars.invoicing.config.CorsFilter;
 
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private CorsFilter corsFilter;
+
+    @Value("${invoicing-system.csrf.disable:false}")
+    private boolean disableCsrf;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
-        http.csrf(csrf -> csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()));
+        http.httpBasic()
+            .and()
+            .addFilterBefore(corsFilter, ChannelProcessingFilter.class);
+
+        if (disableCsrf) {
+            http.csrf().disable();
+        } else {
+            http.csrf(csrf -> csrf.csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()));
+        }
     }
 }
